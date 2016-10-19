5d1706b547c82ad8f80307d708521b7f6db7e882
Nabok <Wladimir.Nabok@haw-hamburg.de>
2016-10-19 15:23:10 +0200

    Added Title to frame

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -6,7 +6,8 @@ import java.io.*;
 import javax.imageio.*;
 
 public class ImageEditorFrame extends JFrame {
-	public ImageEditorFrame(){
+	public ImageEditorFrame(String title){
+		setTitle(title);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(400, 300);
 		setVisible(true);
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
 
 	public static void main (String []args){
-	 ImageEditorFrame imageEditorFrame = new ImageEditorFrame();
+	 ImageEditorFrame imageEditorFrame = new ImageEditorFrame("ImageEditor");
 
 	}
 }
