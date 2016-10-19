e00e91776c1057caeca3623ba5c25b1919c79a2f
Nabok <Wladimir.Nabok@haw-hamburg.de>
2016-10-19 15:06:22 +0200

    Added Frame

---
+++ ImageEditorFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class ImageEditorFrame extends JFrame {
+	public ImageEditorFrame(){
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(400, 300);
+		setVisible(true);
+	}
+}
