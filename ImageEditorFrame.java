066423c5be643123a006279c839d412ac8948656
Irena Becker <irena.becker@gmx.de>
2016-10-19 15:53:32 +0200

    filechooser eingefuegt

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -5,11 +5,26 @@ import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
 
+
 public class ImageEditorFrame extends JFrame {
 	public ImageEditorFrame(String title){
 		setTitle(title);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(400, 300);
 		setVisible(true);
+	}
+
+	private void onOpen(){
+		try{
+			JFileChooser fileChooser = new FileChooser();
+			fileChooser.showOpenDialog(this);
+			File file = fileChooser.getSelectedFile();
+			BufferedImage image = ImageIO.read(file);
+			panel.setImage(image);
+		}
+		catch(IOException e) {
+			JOptionPane.showMessageDialog(this, "Die Datei konnte nicht geöffnet werden");
+		}
+	
 	}
 }
